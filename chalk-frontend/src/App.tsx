e7d1a9267259ccf5620add3e1ec2dbd0b3a20f48
michael-m-2983 <michaelmccright2983@gmail.com>
2026-07-24 11:35:52 -0500

    Glue together frontend and backend

--- chalk-frontend/src/App.tsx
+++ chalk-frontend/src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
 import { Editor } from "./components/Editor";
 
+/**
+ * Basic API test component
+ */
+function APITest() {
+  let [str, setStr] = useState<string>("my_string");
+  let [len, setLen] = useState<number | undefined>(undefined);
+
+  let textbox = <input type="text" value={str} onChange={(e) => setStr(e.currentTarget.value)}/>
+
+  useEffect(() => {
+    fetch(`/api/length/${str}`).then(r => r.text()).then(text => {
+      setLen(
+        parseInt(
+          text.replaceAll(/[^0-9]+/g, "")
+        )
+      );
+    });
+  }, [str, setLen]);
+
+  return <p>Length of "{textbox}" is {len || "not defined"}.</p>
+}
+
 export default function App() {
-  return <Editor />
+  return <div>
+    <APITest />
+    <Editor />
+  </div>
 }
--- chalk-frontend/vite.config.ts
+++ chalk-frontend/vite.config.ts
@@ -5,10 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    outDir: "../chalk-backend/src/main/resources/static",
+    emptyOutDir: true
+  },
   server: {
     // It would be bad to leave this enabled in production
     // But it is served as static HTML, so it is never on in production
     allowedHosts: true,
-    host: true
+    host: true,
+
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
   }
 })
